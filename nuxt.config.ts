c6674f9fdb1d72fcb612d591887a43f7fc966acb
hywax <me@hywax.space>
2024-12-07 22:18:30 +0500

    feat: nuxt future

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -57,5 +57,6 @@ export default defineNuxtConfig({
   },
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2024-11-01',
+  future: { compatibilityVersion: 4 },
   devtools: { enabled: true },
 })
