bb7cf7d930188f6b79e671fc206d4dd0aa59812a
hywax <me@hywax.space>
2024-12-07 21:59:59 +0500

    chore: nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,61 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/i18n',
+    '@nuxt/ui',
+  ],
+  runtimeConfig: {
+    nitro: {
+      envPrefix: 'APP_',
+    },
+    githubToken: '',
+  },
+  i18n: {
+    langDir: 'locales',
+    strategy: 'no_prefix',
+    defaultLocale: 'en',
+    vueI18n: 'vue-i18n.options.ts',
+    experimental: {
+      autoImportTranslationFunctions: true,
+    },
+    detectBrowserLanguage: {
+      useCookie: true,
+      alwaysRedirect: true,
+    },
+    locales: [
+      { code: 'en', language: 'en-US', name: 'English', file: 'en-US.json' },
+      { code: 'ru', language: 'ru-RU', name: 'Русский', file: 'ru-RU.json' },
+    ],
+  },
+  ui: {
+    colorMode: true,
+    fonts: true,
+  },
+  fonts: {
+    provider: 'google',
+    families: [
+      {
+        name: 'Geist',
+        provider: 'google',
+        weights: [300, 400, 500, 600, 700, 800, 900],
+      },
+    ],
+  },
+  colorMode: {
+    classSuffix: '',
+  },
+  icon: {
+    customCollections: [
+      { prefix: 'contr', dir: './app/assets/icons' },
+    ],
+    clientBundle: {
+      includeCustomCollections: true,
+      scan: {
+        globInclude: ['app/**/*.{vue,ts}'],
+        globExclude: ['node_modules', 'dist', 'build', 'coverage', 'test', 'tests', '.*'],
+      },
+    },
+  },
+  css: ['~/assets/css/main.css'],
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
 })
