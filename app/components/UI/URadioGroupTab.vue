601841bf5e6e072ceeed16502466494c90e85006
hywax <me@hywax.space>
2024-12-09 00:37:50 +0500

    feat: app radio component

---
+++ app/components/UI/URadioGroupTab.vue
@@ -0,0 +1,23 @@
+<template>
+  <UTabs
+    v-model="modelValue"
+    :items="items"
+    :content="false"
+    :ui="{ indicator: 'bg-[var(--ui-bg)]', trigger: 'px-1 data-[state=active]:text-[var(--ui-text-highlighted)]' }"
+    color="neutral"
+    size="xs"
+    @update:model-value="(modelValue = $event as T)"
+  />
+</template>
+
+<script setup lang="ts" generic="T extends string | number">
+defineProps<{
+  items: {
+    label: string
+    value: T
+    icon?: string
+  }[]
+}>()
+
+const modelValue = defineModel<T>()
+</script>
