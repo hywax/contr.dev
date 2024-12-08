4415c0b9e0bbac561611cdcbf5ac6b1b97477623
hywax <me@hywax.space>
2024-12-09 01:27:09 +0500

    feat: add cloudflare wrangler

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -68,6 +68,9 @@ export default defineNuxtConfig({
     },
   },
   css: ['~/assets/css/main.css'],
+  nitro: {
+    preset: 'cloudflare-pages',
+  },
   compatibilityDate: '2024-11-01',
   future: { compatibilityVersion: 4 },
   devtools: { enabled: true },
