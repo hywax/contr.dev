4a39c3b5df45c37134a13f6a11629b3ee66f4d05
Alex <me@hywax.space>
2024-12-09 19:50:28 +0500

    feat: seo (#10)
    * feat: seo
    
    * feat: meta tags

---
+++ server/routes/site.webmanifest.get.ts
@@ -0,0 +1,27 @@
+export default cachedEventHandler(() => {
+  const appConfig = useAppConfig()
+
+  return {
+    name: appConfig.app.name,
+    short_name: appConfig.app.name,
+    background_color: '#ffffff',
+    display: 'standalone',
+    icons: [
+      {
+        src: '/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/web-app-manifest-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
+  }
+}, {
+  name: 'site.webmanifest',
+  maxAge: 3600, // 1 hour
+})
