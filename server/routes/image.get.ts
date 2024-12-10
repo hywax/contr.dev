522d88f9664cfca4aa70173adb745c4ab31752c4
hywax <me@hywax.space>
2024-12-10 22:41:20 +0500

    feat: image generator

--- server/routes/image.get.ts
+++ server/routes/image.get.ts
@@ -3,7 +3,35 @@ import { getGeneratorSchema } from '#shared/schemas'
 export default eventHandler(async (event) => {
   const t = await useTranslation(event)
   const generatorSchema = getGeneratorSchema(t)
-  const query = await getValidatedQuery(event, generatorSchema.parse)
+  const { repo, ...options } = await getValidatedQuery(event, generatorSchema.parse)
 
-  return query
+  setHeader(event, 'content-type', 'image/svg+xml')
+
+  const blobStorage = useStorage('blob')
+
+  /**
+   * Step 1: Check exists cache image
+   */
+  const imageName = `${repo}-${options.columns}-${options.size}-${options.shape}.svg`
+  if (await blobStorage.hasItem(imageName)) {
+    logger.log(`Get image from cache: ${repo}`)
+    return await blobStorage.getItem(imageName)
+  }
+
+  /**
+   * Step 2: Get contributors
+   */
+  const contributors = await getContributors(repo)
+  if (contributors.length === 0) {
+    return getSVGPlaceholder('test')
+  }
+
+  /**
+   * Step 3: Generate image
+   */
+  const svg = getSVGGridImage(contributors, options)
+
+  await blobStorage.setItem(imageName, svg)
+
+  return svg
 })
